8e6c88ff48ac33b1d4e5f5a3b8b3951330c1fea0
NafisAhnnaf <nafisahnaf@yahoo.com>
2026-03-20 17:13:54 +0600

    middleware issue fixed

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
     // 1. Define which paths are protected
     const isDashboardPage = request.nextUrl.pathname.startsWith('/admin/dashboard');
 
